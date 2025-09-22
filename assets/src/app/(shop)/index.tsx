f571c3021c7628e18b94d32d7f9a334f5b393641
M.Hammad Idrees <iamhammadidrees@gmail.com>
2025-09-22 13:55:45 -0700

    Add ecommerce pages: auth, cart, and shop

---
+++ assets/src/app/(shop)/index.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const Home= {} => {
+    return (
+        <view>
+            <text>Home</text>
+        </view>
+    );
+};
+
+export default HTMLFormElement;
+const styles = StyeSheet.create({})
---
+++ assets/src/app/cart.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function Cart() {
+    return (
+        <view>
+            <text>Cart</text>
+        </view>
+    );
+}
